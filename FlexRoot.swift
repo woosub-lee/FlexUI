1504a983012cc49508a9b88bb5623ffd62e9d10e
woosub-lee <woosub.dev@gmail.com>
2022-06-27 10:02:23 +0900

    feat: root of FlexView

---
+++ FlexRoot.swift
@@ -0,0 +1,24 @@
+//
+//  FlexRoot.swift
+//  FlexUI
+//
+//  Created by 이우섭 on 2022/06/27.
+//
+
+import FlexLayout
+
+public func FlexRoot(container: UIView,
+                     justifyContent: Flex.JustifyContent = .start,
+                     alignItems: Flex.AlignItems = .start,
+                     @FlexViewBuilder _ content: FlexViewContent) -> Flex {
+    let subContents: [FlexView] = content()
+    
+    container.flex
+        .justifyContent(justifyContent)
+        .alignItems(alignItems)
+    
+    for subContent in subContents {
+        subContent.define(container.flex)
+    }
+    return container.flex
+}
