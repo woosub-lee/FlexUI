6006dfb008e6126cf157a8949673ddebdddddc62
woosub-lee <woosub.dev@gmail.com>
2022-06-28 10:20:07 +0900

    feat: example app domain

---
+++ Example/FlexUI/LottoRandomPool.swift
@@ -0,0 +1,33 @@
+//
+//  LottoRandomPool.swift
+//  FlexUI_Example
+//
+//  Created by 이우섭 on 2022/06/28.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+struct LottoNumberPool {
+    private var numbers: Set<Int> = Set(1...45)
+    
+    mutating func getRandomNumber() -> Int? {
+        guard let randomNumber = numbers.randomElement() else { return nil }
+        numbers.remove(randomNumber)
+        return randomNumber
+    }
+}
+
+struct LottoColorPool {
+    private let colors: Set<UIColor> =  Set([
+        .red,
+        .yellow,
+        .black,
+        .green,
+        .blue
+    ])
+    
+    func getRandomColor() -> UIColor? {
+        return colors.randomElement()
+    }
+}
