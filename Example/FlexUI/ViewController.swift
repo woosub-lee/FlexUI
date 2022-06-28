b365da6b8a9ae37a5a2d07d05a7c77d4cbdebdea
woosub-lee <woosub.dev@gmail.com>
2022-06-28 09:46:58 +0900

    feat: base of example App

--- Example/FlexUI/ViewController.swift
+++ Example/FlexUI/ViewController.swift
@@ -13,16 +13,45 @@ import PinLayout
 class ViewController: UIViewController {
     var rootFlexContainer: UIView = UIView()
     
+    var titleLabel: UILabel = UILabel()
+    var refreshButton: UIButton = UIButton()
+    
+    var tableViewArea: UIView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(rootFlexContainer)
+        self.setViewProperties()
         
-        
+        FlexRoot(container: rootFlexContainer, direction: .column) {
+            FlexHStack {
+                FlexItem(view: titleLabel)
+                FlexSpacer().grow(1)
+                FlexItem(view: refreshButton)
+            }
+            .padding(8, 16)
+            
+            FlexSpacer(height: 24)
+            FlexItem(view: tableViewArea).grow(1)
+        }
     }
     
     override func viewDidLayoutSubviews() {
         rootFlexContainer.pin.all(self.view.safeAreaInsets)
         rootFlexContainer.flex.layout()
+    }
+    
+    private func setViewProperties() {
+        self.view.backgroundColor = .white
+        
+        self.titleLabel.text = "FlexUI Example App"
+        self.titleLabel.font = .boldSystemFont(ofSize: 24)
+        
+        self.refreshButton.setTitle("Refresh", for: .normal)
+        self.refreshButton.titleLabel?.font = .systemFont(ofSize: 18)
+        self.refreshButton.setTitleColor(.systemBlue, for: .normal)
+        
+        self.tableViewArea.backgroundColor = .red
     }
 }
 
