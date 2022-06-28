024c1939aefd9bed04035dcc12a7b52961cadada
woosub-lee <woosub.dev@gmail.com>
2022-06-28 09:18:33 +0900

    refactor: clear example app

--- Example/FlexUI/ViewController.swift
+++ Example/FlexUI/ViewController.swift
@@ -11,47 +11,18 @@ import FlexLayout
 import PinLayout
 
 class ViewController: UIViewController {
-
     var rootFlexContainer: UIView = UIView()
-    var titleLabel: UILabel = UILabel()
-    var settingLabel: UILabel = UILabel()
-    var redView: UIView = UIView()
-    var blueView: UIView = UIView()
-    var emptyView: UIView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.redView.backgroundColor = .red
-        self.blueView.backgroundColor = .blue
-        self.titleLabel.text = "Title"
-        self.titleLabel.font = .boldSystemFont(ofSize: 32)
-        self.settingLabel.text = "Settings"
-        self.settingLabel.font = .systemFont(ofSize: 24)
-        
-        
         self.view.addSubview(rootFlexContainer)
-        FlexRoot(container: rootFlexContainer, direction: .column) {
-            FlexHStack {
-                FlexItem(view: titleLabel)
-                FlexSpacer().grow(1)
-                FlexItem(view: settingLabel)
-            }.padding(0, 8)
-            FlexItem(view: redView).grow(1)
-        }.backgroundColor(.lightGray)
         
-        self.rootFlexContainer.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(update)))
+        
     }
-
+    
     override func viewDidLayoutSubviews() {
         rootFlexContainer.pin.all(self.view.safeAreaInsets)
         rootFlexContainer.flex.layout()
     }
-    
-    @objc func update() {
-        rootFlexContainer.flex.markDirty()
-        rootFlexContainer.setNeedsLayout()
-        rootFlexContainer.layoutIfNeeded()
-    }
-
 }
 
